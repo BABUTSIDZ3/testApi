dd1bc1e6a75111418163af622e2984fe2fdefe29
BABUTSIDZ3 <babutsidzegia7@gmail.com>
2024-06-26 13:03:22 +0400

    cw

--- controllers/questions.js
+++ controllers/questions.js
@@ -168,7 +168,7 @@ questionsRouter.post("/answer", async (req, res) => {
       ? question[0].right_answer_GE
       : question[0].right_answer_EN === answer
   ) {
-    res.send(`use-x: ${use_x}`);
+    res.send("Your answer is correct");
     await queryDatabase(coinAddQuerry, [coinToAdd, user_id]);
   } else {
     await queryDatabase(incorrectAnswerQuerry, [user_id]);
