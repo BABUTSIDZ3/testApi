6e565469e697b294ca2634b3678f23d44faa78d8
BABUTSIDZ3 <babutsidzegia7@gmail.com>
2024-07-03 13:26:08 +0400

    btr

--- utils/functions.js
+++ utils/functions.js
@@ -1,4 +1,3 @@
-
 import { conection } from "../app.js";
 
 // Function to handle database queries
@@ -18,7 +17,7 @@ export async function levelup(req, res) {
   try {
     const runLevelUp = async () => {
       try {
-        let round=0
+        let round = 0;
         const sql_query =
           "SELECT id,balance,balancetobecollected,subscription,paydonlevel,level FROM users";
         const results = await queryDatabase(sql_query);
@@ -39,7 +38,7 @@ export async function levelup(req, res) {
         for (const level of Object.keys(groupedResult)) {
           const response = groupedResult[level];
           if (response.length === 2) {
-            round=1
+            round = 1;
             const totalBalancetobecollected = response.reduce(
               (total, user) => total + Number(user.balancetobecollected),
               0
@@ -47,24 +46,24 @@ export async function levelup(req, res) {
             const balancetobecollectedonlevel = totalBalancetobecollected * 0.8;
             let forbalance = balancetobecollectedonlevel * 0.1;
             const forbalancetobecollected = balancetobecollectedonlevel * 0.9;
-            const levelquerry=`SELECT id FROM users WHERE level=?`
-const allUsersOnSameLevel=await queryDatabase(levelquerry,[level])
-console.log(allUsersOnSameLevel);
+            const levelquerry = `SELECT id FROM users WHERE level=?`;
+            const allUsersOnSameLevel = await queryDatabase(levelquerry, [
+              level,
+            ]);
             const userToLevelUp = response.sort((a, b) => a.id - b.id)[0];
 
             if (userToLevelUp.subscription === 0) {
               forbalance /= 2;
             }
 
-            const updatepaydonlevelQuery = `UPDATE users SET balancetobecollected = 0, paydonlevel = 1 WHERE level = ${level} AND paydonlevel = 0`;
+            const updatepaydonlevelQuery = `UPDATE users SET balancetobecollected = 0, paydonlevel = 1 WHERE level = ${allUsersOnSameLevel[0].id} AND paydonlevel = 0`;
             await queryDatabase(updatepaydonlevelQuery);
 
             const updateUserQuery = `UPDATE users SET paydonlevel=0, balance = balance + ${forbalance}, balancetobecollected = balancetobecollected + ${forbalancetobecollected}, level = level + 1 WHERE id = ${userToLevelUp.id}`;
             await queryDatabase(updateUserQuery);
           }
         }
-        if(round!==0)
-          await runLevelUp()
+        if (round !== 0) await runLevelUp();
       } catch (error) {
         res.send(error.message);
       }
@@ -75,5 +74,3 @@ console.log(allUsersOnSameLevel);
     res.send(error.message);
   }
 }
-
-
